87b91f49c00684ca07fc714b1eeefac58e892260
Gerardo Gallegos <gerardg-dev@gmail.com>
2020-12-07 15:42:24 -0600

    redux store setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "./store/configureStore";
 import moment from "moment";
-import "./App.css";
 import Calendar from "./components/Calendar/index.js";
+import "./App.css";
+
+const store = configureStore();
+
+console.log(store.getState());
 
 function App() {
   const [value, setValue] = useState(moment());
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Calendar value={value} onChange={setValue} />
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <Calendar value={value} onChange={setValue} />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
